9bd9fa5e7ef3275a0daa08a61bba021afd350788
RimmaO <rimma_o@ukr.net>
2023-06-21 16:46:29 +0300

    update 01

--- src/components/01Profile/Profile.jsx
+++ src/components/01Profile/Profile.jsx
@@ -6,22 +6,24 @@ export const Profile = ({ avatar, username, tag, location, stats }) => {
     <div className={css.profile}>
       <div className={css.description}>
         <img src={avatar} alt="User avatar" className={css.avatar} />
-        <p className={css.name}>{username}</p>
-        <p className={css.tag.text}>@{tag}</p>
-        <p className={css.location.text}>{location}</p>
+        <p className={css.name}>
+          <b>{username}</b>
+        </p>
+        <p className={css.tag}>@{tag}</p>
+        <p className={css.location}>{location}</p>
       </div>
 
       <ul className={css.stats}>
-        <li>
-          <span className={css.label.text}>Followers </span>
+        <li className={css.item}>
+          <span className={css.label}>Followers </span>
           <span className={css.quantity}>{stats.followers}</span>
         </li>
-        <li>
-          <span className={css.label.text}>Views </span>
+        <li className={css.item}>
+          <span className={css.label}>Views </span>
           <span className={css.quantity}>{stats.views}</span>
         </li>
-        <li>
-          <span className={css.label.text}>Likes </span>
+        <li className={css.item}>
+          <span className={css.label}>Likes </span>
           <span className={css.quantity}>{stats.likes}</span>
         </li>
       </ul>
