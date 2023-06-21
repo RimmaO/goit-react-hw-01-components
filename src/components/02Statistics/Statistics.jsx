f8cdda106eb9f06afbcb36f87454c51e708a2aec
RimmaO <rimma_o@ukr.net>
2023-06-21 15:20:47 +0300

    up

---
+++ src/components/02Statistics/Statistics.jsx
@@ -0,0 +1,37 @@
+// import PropTypes from 'prop-types';
+
+// const Statistics = ({ title, stats }) => {
+//   return (
+//     <section className="statistics">
+//       <h2 className="title">Upload stats</h2>
+
+//       <ul className="stat-list">
+//         <li className="item">
+//           <span className="label">.docx</span>
+//           <span className="percentage">4%</span>
+//         </li>
+//         <li className="item">
+//           <span className="label">.mp3</span>
+//           <span className="percentage">14%</span>
+//         </li>
+//         <li className="item">
+//           <span className="label">.pdf</span>
+//           <span className="percentage">41%</span>
+//         </li>
+//         <li className="item">
+//           <span className="label">.mp4</span>
+//           <span className="percentage">12%</span>
+//         </li>
+//       </ul>
+//     </section>
+//   );
+// };
+
+// Statistics.propTypes = {
+//   title: PropTypes.string,
+//   stats: PropTypes.shape({
+//     followers: PropTypes.number.isRequired,
+//     views: PropTypes.number.isRequired,
+//     likes: PropTypes.number.isRequired,
+//   }),
+// };
